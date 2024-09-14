76a5c521a062978628d582adb52e9a4d927103b7
KavinduGD <it21038914@my.sliit.lk>
2024-09-15 00:35:53 +0530

    bug test

--- backend/controllers/busController.js
+++ backend/controllers/busController.js
@@ -2,109 +2,8 @@ const Bus = require("../models/busModel");
 const asyncHandler = require("express-async-handler");
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
-const { fileSizeFormatter } = require("../util/fileUpload");
-//create bus
-// const createBus = asyncHandler(async (req, res) => {
-//   const personType = req.personType;
-
-//   if (personType === "user") {
-//     res.status(401);
-//     throw new Error("Not authorized , Please login as a manager");
-//   }
-
-//   const {
-//     busId,
-//     registrationNumber,
-//     chassisNumber,
-//     model,
-//     seatingCapacity,
-//     color,
-//     driver,
-//     conductor,
-//     owner,
-//   } = req.body;
-
-//   //validate
-//   if (
-//     !busId ||
-//     !registrationNumber ||
-//     !chassisNumber ||
-//     !model ||
-//     !seatingCapacity ||
-//     !color ||
-//     !driver ||
-//     !conductor ||
-//     !owner
-//   ) {
-//     if (req.file) {
-//       fs.unlinkSync(req.file.path);
-//     }
-//     res.status(400);
-//     throw new Error("Please fill all the fields");
-//   }
-
-//   //Manage image upload
-//   let fileData = {};
-//   let uploadedFile;
-
-//   if (req.file) {
-//     //upload
-//     try {
-//       uploadedFile = await cloudinary.uploader.upload(req.file.path, {
-//         folder: "KTS/buses",
-//         resource_type: "image",
-//       });
-//     } catch (err) {
-//       res.status(500);
-//       throw new Error("Image could not be uploaded");
-//     }
-
-//     fileData = {
-//       fileName: req.file.originalname,
-//       filePath: uploadedFile.secure_url,
-//       fileType: req.file.mimetype,
-//       fileSize: fileSizeFormatter(req.file.size, 2),
-//       fileID: uploadedFile.public_id,
-//     };
-
-//     //delete file from uploads folder
-//     fs.unlinkSync(req.file.path);
-//   }
-
-//   try {
-//     const bus = await Bus.create({
-//       busId,
-//       registrationNumber,
-//       chassisNumber,
-//       model,
-//       seatingCapacity,
-//       color,
-//       driver,
-//       conductor,
-//       owner,
-//       photo: fileData,
-//     });
-
-//     if (bus) {
-//       res.status(201).json(bus);
-//     } else {
-//       if (fileData.filePath) {
-//         await cloudinary.uploader.destroy(uploadedFile.public_id);
-//       }
-//       res.status(400);
-//       throw new Error("Could not create bus");
-//     }
-//   } catch (err) {
-//     if (fileData.filePath) {
-//       await cloudinary.uploader.destroy(uploadedFile.public_id);
-//     }
-//     res.status(500);
-//     throw new Error(err);
-//   }
-// });
-
-
 const path = require('path');
+const { fileSizeFormatter } = require("../util/fileUpload");
 
 // Assuming 'uploads' is the directory where files are supposed to be uploaded
 const UPLOAD_DIRECTORY = path.join(__dirname, '../uploads');
@@ -114,7 +13,7 @@ function isSafePath(filePath) {
     return resolvedPath.startsWith(UPLOAD_DIRECTORY);
 }
 
-// Within your function
+//create bus
 const createBus = asyncHandler(async (req, res) => {
   const personType = req.personType;
 
@@ -166,6 +65,9 @@ const createBus = asyncHandler(async (req, res) => {
         resource_type: "image",
       });
     } catch (err) {
+      if (isSafePath(req.file.path)) {
+        fs.unlinkSync(req.file.path);
+      }
       res.status(500);
       throw new Error("Image could not be uploaded");
     }
