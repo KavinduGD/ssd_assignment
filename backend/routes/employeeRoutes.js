ad8b02a79b691bd726d3d3cb5c32c3a025531ee8
KavinduGD <it21038914@my.sliit.lk>
2024-09-16 14:42:09 +0530

    cc

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 // Create a rate limiter
 const createAccountLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 150, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
