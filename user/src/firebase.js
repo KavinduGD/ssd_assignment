8f63afa25e07bc23e159da82170c49567329be19
KavinduGD <it21038914@my.sliit.lk>
2024-09-16 13:16:34 +0530

    firebase api key hidden

--- user/src/firebase.js
+++ user/src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBx9TFD1jcOdi9aKyOngWjLcNhBJ7Fgpy4",
+  apiKey: import.meta.env.VITE_VITE_FIREBASE_API_KEY,
   authDomain: "kts-fireproject.firebaseapp.com",
   projectId: "kts-fireproject",
   storageBucket: "kts-fireproject.appspot.com",
