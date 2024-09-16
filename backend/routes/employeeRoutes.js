57847218b10d0d2c08212830ec43550db88db52d
KavinduGD <it21038914@my.sliit.lk>
2024-09-16 14:38:17 +0530

    dos attack bug fix employee

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -9,10 +9,20 @@ const {
 } = require("../controllers/employeeController");
 const protect = require("../middleware/authMiddleware");
 const { upload } = require("../util/fileUpload");
+const rateLimit = require("express-rate-limit");
+
 const router = express.Router();
 
+// Create a rate limiter
+const createAccountLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+
 //create employee  -- manager
-router.post("/", protect, upload.single("image"), createEmployee);
+router.post("/", protect,createAccountLimiter, upload.single("image"), createEmployee);
 //get all employees
 router.get("/", getAllEmployees);
 //get a single employee
